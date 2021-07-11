6fcb876c2b827a52d0f61aa5e3c428681e72f313
Ahmad Almunajjed <AhmadAlMunajjed@outlook.com>
2021-07-11 21:51:20 +0800

    updates

--- layout/footer.php
+++ layout/footer.php
@@ -10,19 +10,19 @@
             </div>
             <div class="col-lg-4">
                 <div class="feature-inner hvr-shrink">
-                    <img src="../assets/images/home/f1.png" style="max-width: 75px;" class="img-responsive" alt=""/>
+                    <img src="../assets/images/home/f1.png" style="max-width: 75px;" class="img-responsive" alt="" />
                     <h4>LATEST INFORMATION HERE!</h4>
                 </div>
             </div>
             <div class="col-lg-4">
                 <div class="feature-inner hvr-shrink">
-                    <img src="../assets/images/home/f2.png" style="max-width: 75px;" class="img-responsive" alt=""/>
+                    <img src="../assets/images/home/f2.png" style="max-width: 75px;" class="img-responsive" alt="" />
                     <h4>Have you registered? If not, do register now to protect yourself and your loved ones.</h4>
                 </div>
             </div>
             <div class="col-lg-4">
                 <div class="feature-inner hvr-shrink">
-                    <img src="../assets/images/home/f3.png" style="max-width: 75px;" class="img-responsive" alt=""/>
+                    <img src="../assets/images/home/f3.png" style="max-width: 75px;" class="img-responsive" alt="" />
                     <h4>See ;atest news about Covid-19</h4>
                 </div>
             </div>
@@ -32,22 +32,25 @@
 
 <footer>
     <div class="container">
- <div class="row">				
+        <div class="row">
             <div class="col-lg-12">
                 <div class="copy-right">
-				<h4>Copyright © 2021  All rights reserved.  </h4>                
+                    <h4>Copyright © 2021 All rights reserved. </h4>
                 </div>
             </div>
         </div>
     </div>
 </footer>
 </div>
-<script> base_url = "index.php"; </script>
+<script>
+    base_url = "index.php";
+</script>
 <!-- jQuery 2.1.4 -->
-    <script src="../assets/js/jquery.js"></script>
-	<script src="../assets/js/jquery-1.9.1.min.js"></script>
-	<script src="../assets/js/bootstrap.min.js"></script>
-	<script src="../assets/js/script.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;key=AIzaSyAPkwBOzGBH1V1sRBzmCWQS-7XoTgPghT0&amp;libraries=places"></script>
+<script src="../assets/js/jquery.js"></script>
+<script src="../assets/js/jquery-1.9.1.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/script.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;key=AIzaSyAPkwBOzGBH1V1sRBzmCWQS-7XoTgPghT0&amp;libraries=places"></script>
 </body>
+
 </html>
--- layout/header.php
+++ layout/header.php
@@ -1,57 +1,60 @@
 <!DOCTYPE html>
-	<html>
+<html>
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <meta charset="UTF-8">
-	<title>getYourVaccine</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>	
-	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../assets/css/font-awesome.min.css">    
-	<link rel="stylesheet" href="maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-	<link rel="stylesheet" href="../assets/css/animate.css">
-	<link rel="stylesheet" href="../assets/css/hover-min.css"> 
+<title>Covid-19 vaccination</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+<link rel="stylesheet" href="maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<link rel="stylesheet" href="../assets/css/animate.css">
+<link rel="stylesheet" href="../assets/css/hover-min.css">
 <!--common-head-css -->
-	<link rel="stylesheet" href="../assets/css/style.css">
-	<link rel="stylesheet" href="../assets/css/responsive.css">
+<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="../assets/css/responsive.css">
 
-		<body class="hold-transition  sidebar-mini">
-			<div class="wrapper">
-				<nav class="navbar navbar-default nav-main">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-xs-4 col-sm-12 logo-col">
-                <div class="logo animated fadeInLeft">
-                    <h1><a   style="padding:15px;background-color:#3CB64C;color:white; font-family: Georgia Header;border-radius:50px 0px 50px 0px" href="index.php">getYourVaccine</a></h1>
+<body class="hold-transition  sidebar-mini">
+    <div class="wrapper">
+        <nav class="navbar navbar-default nav-main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-xs-4 col-sm-12 logo-col">
+                        <div class="logo animated fadeInLeft">
+                            <h1>
+                                <a style="padding:15px;background-color:#3CB64C;color:white; font-family: Georgia Header;border-radius:50px 0px 50px 0px" href="index.php">
+                                    Covid-19 Vac
+                                </a>
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="col-lg-9 col-xs-8 col-sm-12 header-sgnin">
+                        <div class="signin">
+                            <ul>
+                                <li><a href="check.php"><img src="../assets/images/home/2.png" />Check</a></li>
+                                <li><a href="register.php"><img src="../assets/images/home/1.png" />Regiester</a></li>
+                            </ul>
+                        </div>
+                        <div class="clearfix hidden-xs"></div>
+                        <div class="navbar-header navbar-header-resp">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav nav-head animated fadeInDown">
+                                <li class="active m-1"><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
+                                <li><a href="aboutus.php">About the vaccine</a></li>
+                                <li><a href="contact.php">Contact </a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-9 col-xs-8 col-sm-12 header-sgnin">
-                <div class="signin">
-                    <ul>
-						<li ><a href = "check.php"><img src="../assets/images/home/2.png" />Check</a></li>
-						<li ><a href = "register.php"><img src="../assets/images/home/1.png" />Regiester</a></li>
-					</ul>
-                </div>
-                <div class="clearfix hidden-xs"></div>
-                <div class="navbar-header navbar-header-resp">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav nav-head animated fadeInDown">
-                        <li class="active m-1"><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
-                        <li><a href = "aboutus.php">About the vaccine</a></li>
-						<li><a href = "faq.php">FAQ </a></li>
-                        <li><a href = "contact.php">Contact </a></li>
-                    </ul>
-                </div>
-            </div>
+        </nav>
+        <div class="container">
+
         </div>
-    </div>
-</nav>
-<div class="container">
-
-</div>

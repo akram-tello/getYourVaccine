875a3c4934d5789544daf89ed78d5b669df991f9
Ahmad Almunajjed <AhmadAlMunajjed@outlook.com>
2021-07-11 23:14:29 +0800

    applications

---
+++ applications.php
@@ -0,0 +1,238 @@
+<?php include("layout/header.php"); ?>
+<div class="container-fluid about-bac-another">
+    <div class="container">
+        <h1 class="text-center textblue" style="padding-bottom: 2em;">Applications</h1>
+
+        <div class="mb-4">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Applicant</th>
+                        <th>Address</th>
+                        <th>Registertion date</th>
+                        <th>1st dose date</th>
+                        <th>2nd dose date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="table-success">
+                        <th class="textblue">
+                            Full name
+                            <br>
+                            015646564564
+                            <br>
+                            email@example.com
+                            <br>
+                            IC: 2646546465
+                        </th>
+                        <td>
+                            State
+                            <br>
+                            City, Postalcode
+                            <br>
+                            Adress details
+                            <br>
+                        </td>
+                        <td>11-07-2021</td>
+                        <td>11-08-2021</td>
+                        <td>10-09-2021</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Set dose date
+                            </button>
+                        </td>
+                    </tr>
+                    <tr class="table-success">
+                        <th class="textblue">
+                            Full name
+                            <br>
+                            015646564564
+                            <br>
+                            email@example.com
+                            <br>
+                            IC: 2646546465
+                        </th>
+                        <td>
+                            State
+                            <br>
+                            City, Postalcode
+                            <br>
+                            Adress details
+                            <br>
+                        </td>
+                        <td>11-07-2021</td>
+                        <td>11-08-2021</td>
+                        <td>10-09-2021</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Set dose date
+                            </button>
+                        </td>
+                    </tr>
+                    <tr class="table-success">
+                        <th class="textblue">
+                            Full name
+                            <br>
+                            015646564564
+                            <br>
+                            email@example.com
+                            <br>
+                            IC: 2646546465
+                        </th>
+                        <td>
+                            State
+                            <br>
+                            City, Postalcode
+                            <br>
+                            Adress details
+                            <br>
+                        </td>
+                        <td>11-07-2021</td>
+                        <td>11-08-2021</td>
+                        <td>10-09-2021</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Set dose date
+                            </button>
+                        </td>
+                    </tr>
+                    <tr class="table-success">
+                        <th class="textblue">
+                            Full name
+                            <br>
+                            015646564564
+                            <br>
+                            email@example.com
+                            <br>
+                            IC: 2646546465
+                        </th>
+                        <td>
+                            State
+                            <br>
+                            City, Postalcode
+                            <br>
+                            Adress details
+                            <br>
+                        </td>
+                        <td>11-07-2021</td>
+                        <td>11-08-2021</td>
+                        <td>10-09-2021</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Set dose date
+                            </button>
+                        </td>
+                    </tr>
+                    <tr class="table-success">
+                        <th class="textblue">
+                            Full name
+                            <br>
+                            015646564564
+                            <br>
+                            email@example.com
+                            <br>
+                            IC: 2646546465
+                        </th>
+                        <td>
+                            State
+                            <br>
+                            City, Postalcode
+                            <br>
+                            Adress details
+                            <br>
+                        </td>
+                        <td>11-07-2021</td>
+                        <td>11-08-2021</td>
+                        <td>10-09-2021</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Set dose date
+                            </button>
+                        </td>
+                    </tr>
+                    <tr class="table-success">
+                        <th class="textblue">
+                            Full name
+                            <br>
+                            015646564564
+                            <br>
+                            email@example.com
+                            <br>
+                            IC: 2646546465
+                        </th>
+                        <td>
+                            State
+                            <br>
+                            City, Postalcode
+                            <br>
+                            Adress details
+                            <br>
+                        </td>
+                        <td>11-07-2021</td>
+                        <td>11-08-2021</td>
+                        <td>10-09-2021</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Set dose date
+                            </button>
+                        </td>
+                    </tr>
+                    <tr class="table-success">
+                        <th class="textblue">
+                            Full name
+                            <br>
+                            015646564564
+                            <br>
+                            email@example.com
+                            <br>
+                            IC: 2646546465
+                        </th>
+                        <td>
+                            State
+                            <br>
+                            City, Postalcode
+                            <br>
+                            Adress details
+                            <br>
+                        </td>
+                        <td>11-07-2021</td>
+                        <td>11-08-2021</td>
+                        <td>10-09-2021</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                                Set dose date
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                appointment info
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
+
+<!--footer-->
+<?php include("layout/footer.php"); ?>
